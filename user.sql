c07777b84fbb15c90aee5770b74ff2616cfe1a00
st52938 <43779295+filipmanhal@users.noreply.github.com>
2018-10-28 15:22:48 +0100

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE user
+(
+  id          INT AUTO_INCREMENT
+    PRIMARY KEY,
+  USERNAME    VARCHAR(20) NOT NULL,
+  password    VARCHAR(20) NOT NULL,
+  email       VARCHAR(65) NOT NULL,
+  description TEXT        NULL,
+  created     DATETIME    NULL,
+  CONSTRAINT user_USERNAME_uindex
+  UNIQUE (USERNAME),
+  CONSTRAINT user_email_uindex
+  UNIQUE (email)
+)
+  ENGINE = InnoDB;
+
+
